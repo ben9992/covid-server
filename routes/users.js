6ceef3e988860774115f1db783395ac36b2215d4
Ben Mishali <you@example.com>
2023-02-27 07:56:13 +0200

    ..

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,14 @@
 const express = require("express"),
 	router = express.Router();
 
-router.get("/", async (req, res) => {
-	res.status(200).send("fsgass");
+// Add user
+router.post("/user", (req, res) => {
+	const { username } = req.body;
+	if (!username) {
+		return res.status(400).json({ error: "Username is required" });
+	}
+	userList[username] = [];
+	return res.json({ success: true });
 });
 
 module.exports = router;
